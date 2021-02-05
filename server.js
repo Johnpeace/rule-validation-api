a7176519b6b6baa04aed365c45c8ac63af7bde96
Ropo Olatujoye <ropo@nairabox.com>
2021-02-05 11:01:12 +0100

    clean up

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
-import Debug from "debug";
-import { config } from "dotenv";
+import Debug from 'debug';
+import { config } from 'dotenv';
 
-const debug = Debug("http");
+const debug = Debug('http');
 
-process.on("uncaughtException", (error) => {
-  debug("UNCAUGHT EXCEPTION! 💥 Shutting down...");
+process.on('uncaughtException', (error) => {
+  debug('UNCAUGHT EXCEPTION! 💥 Shutting down...');
   debug(error.name, error.message);
   process.exit(1);
 });
 
-config({ path: "./config.env" });
-import app from "./server/app";
+config({ path: './config.env' });
+import app from './server/app';
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   debug(`App running on port ${port}...`);
 });
 
-process.on("unhandledRejection", (error) => {
-  debug("UNHANDLED REJECTION! Shutting down...");
+process.on('unhandledRejection', (error) => {
+  debug('UNHANDLED REJECTION! Shutting down...');
   debug(error.name, error.message);
   server.close(() => {
     process.exit(1);
   });
 });
+
+export default server;
